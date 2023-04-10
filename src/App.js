31f2ed5c31b3b1e58d79f07e4700b8712a98ca77
soufiane irssal <soufiane.irssal@gmail.com>
2023-04-10 17:54:45 +0200

    polish

--- src/App.js
+++ src/App.js
@@ -7,7 +7,7 @@ function App() {
   const [count, setCount] = useState(0);
   const [countChart, setCountChart] = useState(0);
   const [open, setOpen] = useState(false);
-  const [openMenu, setOpenMenu] = useState(false);
+  const [openMenu, setOpenMenu] = useState(true);
   let menuRef = useRef();
   let menu2Ref = useRef();
 
